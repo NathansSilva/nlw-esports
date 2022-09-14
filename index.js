a3e2203b591ebe2a1f0331ac9f55c9b59fdc3705
Nathan <Nathansilva18@outlook.com>
2022-09-13 23:55:27 -0300

    first commit - aula 2

---
+++ index.js
@@ -0,0 +1,15 @@
+// const tree = [
+//   {
+//     value: 2,
+//     children: [
+//       { value: 1, children: [] },
+//       {
+//         value: 3,
+//         children: [
+//           { value: 4, children: [] },
+//           { value: 5, children: [] },
+//         ],
+//       },
+//     ],
+//   },
+// ];
